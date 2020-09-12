51a08b71691f8b70f0064adb344b6ecaf4ebcf64
ArcaneCiCi <59527329+ArcaneCiCi@users.noreply.github.com>
2020-09-12 01:54:37 -0400

    Add files via upload

---
+++ src/gg/arcanecici/colorutils/ColorUtils.java
@@ -0,0 +1,44 @@
+package gg.arcanecici.colorutils;
+
+public enum ColorUtils {
+    BLACK("\033[0;30m"),
+    RED("\033[0;31m"),
+    GREEN("\033[0;32m"),
+    YELLOW("\033[0;33m"),
+    BLUE("\033[0;34m"),
+    PURPLE("\033[0;35m"),
+    CYAN("\033[0;37m"),
+    WHITE("\033[0;37m"),
+    RESET("\033[m"),
+    BLACK_BOLD("\033[1;30m"),
+    RED_BOLD("\033[1;31m"),
+    GREEN_BOLD("\033[1;32m"),
+    YELLOW_BOLD("\033[1;33m"),
+    BLUE_BOLD("\033[1;34m"),
+    PURPLE_BOLD("\033[1;35m"),
+    CYAN_BOLD("\033[1;36m"),
+    WHITE_BOLD("\033[1;37m"),
+    BLACK_UNDERLINED("\033[4;30m"),
+    RED_UNDERLINED("\033[4;31m"),
+    GREEN_UNDERLINED("\033[4;32m"),
+    YELLOW_UNDERLINED("\033[4;33m"),
+    BLUE_UNDERLINED("\033[4;34m"),
+    PURPLE_UNDERLINED("\033[4;35"),
+    CYAN_UNDERLINED("\033[4;36m"),
+    WHITE_UNDERLINED("\033[4;37m"),
+    BLACK_BACKGROUND("\033[40m"),
+    RED_BACKGROUND("\033[41m"),
+    GREEN_BACKGROUND("\033[42m"),
+    YELLOW_BACKGROUND("\033[43m"),
+    BLUE_BACKGROUND("\033[44m");
+
+    private final String colorCode;
+
+    ColorUtils(String colorCode) {
+        this.colorCode = colorCode;
+    }
+
+    public String getColor() {
+        return colorCode;
+    }
+}
